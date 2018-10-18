1ffe8079f3bfd8e00822d531f3cd9eb3e03eece4
bry-an <bbyunis@gmail.com>
2018-10-17 20:50:16 -0600

    new header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -9,7 +9,8 @@ const Header =  () => (
         <Link style={{textDecoration: 'none'}} to='/'>
         <img id='header-img' src='https://pmcvariety.files.wordpress.com/2013/08/t_logo_2048_black.png?w=1000&h=563&crop=1' alt='nyt-logo' />
         <div id='header-text'>
-        <h1>NYT Search</h1>
+        <h1>Clean NYT</h1>
+        <h3><em>Find what you want. Read it, or save it for later. <br/>No funny business.</em></h3>
         </div>
         </Link>
         <div id='nav'>
