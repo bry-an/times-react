e2c7ea4d05ca182f3837df35432589f6c9ca0edb
bry-an <bbyunis@gmail.com>
2019-04-03 11:33:47 -0600

    remove reference to userService

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const articleRoutes = require('./articles')
-const userRoutes = require('./users')
+// const userRoutes = require('./users')
 
 router.use('/articles', articleRoutes)
-router.use('/users', userRoutes)
+// router.use('/users', userRoutes)
 
 module.exports = router
